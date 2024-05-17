026f086a40d62eaaa8e588bd23ce7465b575d115
verazizzo <verazizzo@users.noreply.github.com>
2024-05-17 10:00:51 +0200

    Fix edge cases submit button

--- src/scripts/service-worker.ts
+++ src/scripts/service-worker.ts
@@ -1,6 +1,6 @@
 import { formQuestions } from "../types/formQuestions";
 
-function getValues(){
+function getPrefFromStorage(){
     chrome.storage.local.get(["formValue"]).then((result) => {
         let formQuestion = result.formValue as formQuestions
         sendMessage(formQuestion).catch(err=>console.log(err)).then((res)=>console.log(res));
@@ -14,8 +14,8 @@ async function sendMessage(formQuestion : formQuestions){
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request == "open"){
-            getValues()
-        } else{
+            getPrefFromStorage()
+        } else {
             let formData = request.formData as formQuestions
             chrome.storage.local.set({"formValue": formData}, () => {
             });
